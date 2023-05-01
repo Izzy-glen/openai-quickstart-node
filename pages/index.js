13761484baa06c2e39a3d16f1d4596f785f7325b
Glenhalton Takor <33781482+Izzy-glen@users.noreply.github.com>
2023-05-01 00:05:21 +0000

    first commit

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
+        <title>Jarvis AI</title>
         <link rel="icon" href="/dog.png" />
       </Head>
 
